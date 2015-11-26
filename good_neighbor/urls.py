3ab41419cc6133e30a13d8edd64d25014c714c65
Alex Raldugin <sanders777@mail.ru>
2015-11-26 14:52:45 +0300

    base configuration

--- good_neighbor/urls.py
+++ good_neighbor/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from good_neighbor_app.views import index, tasks, neighbors
+
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', index),
+    url(r'^tasks/$', tasks),
+    url(r'^neighbors/$', neighbors),
 ]
--- good_neighbor_app/views.py
+++ good_neighbor_app/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 from models import *
-from mongoengine import *
-
 # Create your views here.
 
 
-def test_view(request):
-    connect('good_neighbor_db')
-    article = User(username='Jhon')
-    article.save()
-    return HttpResponse('<h1>Saved!<h1>')
+def index(request):
+    return render_to_response('index.html')
+
+
+def tasks(request):
+    return render_to_response('tasks.html')
+
+
+def neighbors(request):
+    return render_to_response('neighbors.html')
